4633ddb167ab2644cc895ad810df8253614d8c77
Velican EVLİ <veli-can-evli06@hotmail.com>
2023-04-01 16:44:55 +0300

    emailjs added

--- src/App.js
+++ src/App.js
@@ -4,16 +4,20 @@ import Analytics from "./components/Analytics";
 import Newsletter from "./components/Newsletter";
 import Cards from "./components/Cards";
 import Footer from "./components/Footer";
+import Contact from "./components/Contact";
+import { ToastContainer } from "react-toastify";
 
 function App() {
   return (
     <div className="App">
-      <Navbar/>
-      <Hero/>
-      <Analytics/>
-      <Newsletter/>
-      <Cards/>
-      <Footer/>
+  
+      <Navbar />
+      <Hero />
+      <Analytics />
+      <Newsletter />
+      <Cards />
+      <Contact />
+      <Footer />
     </div>
   );
 }
